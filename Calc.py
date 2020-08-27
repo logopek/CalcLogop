bb3ef51fbc42342028d892f55ddeb61ec994577b
logopek <49311518+logopek@users.noreply.github.com>
2020-08-27 12:27:21 +0300

    Add files via upload

---
+++ Calc.py
@@ -0,0 +1,18 @@
+a=int(input("Your first number:"))
+b=int(input("Your second number:"))
+x=input("+, -, /, *, **, //, %:")
+
+if x=="+":
+	print(a+b)
+if x=="-":
+	print(a-b)
+if x=="/":
+	print(a/b)
+if x=="*":
+	print(a*b)
+if x=="**":
+	print(a**b)
+if x=="//":
+	print(a//b)
+if x=="%":
+	print(a%b)
